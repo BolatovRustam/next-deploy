250e0ab7d4e52e637619df9fcd4437fdafb9b8e5
Bolatov_Rustam <bolatov-rustam2001@mail.ru>
2026-04-17 01:13:04 +0500

    add Prisma Postgres + Accelerate

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,18 +1,19 @@
 import "server-only";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+import { PrismaClient } from "../generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
+    prisma?: ReturnType<typeof createPrismaClient>;
 };
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+function createPrismaClient() {
+    return new PrismaClient({
+        accelerateUrl: process.env.DATABASE_URL!,
+    }).$extends(withAccelerate());
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+    globalForPrisma.prisma = prisma;
 }
