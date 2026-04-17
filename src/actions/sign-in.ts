090bd1b9a7b7b70d574c8c1cb08204821e579423
Bolatov_Rustam <bolatov-rustam2001@mail.ru>
2026-04-17 11:31:49 +0500

    sign-in log

--- src/actions/sign-in.ts
+++ src/actions/sign-in.ts
@@ -3,12 +3,17 @@
 import { signIn } from "@/auth/auth";
 
 export async function signInWithCredentials(email: string, password: string) {
+    console.log("SIGN IN ATTEMPT:", { email });
+
     try {
+        
         await signIn( "credentials", {
             email,
             password,
             redirect: false
         });
+
+        console.log("SIGN IN SUCCESS:", { email });
 
         return
     } catch (error) {
